24359e08f2c973859c11c96531b9a00b6bf52421
Luis Esquivel <lesquivel71@gmail.com>
2015-05-18 07:48:30 -0600

    First pass on styles

--- js/songlist.js
+++ js/songlist.js
@@ -7,6 +7,8 @@ $(document).ready(function() {
 
 var songs = {
   songList : null,
+  editing: false,
+  counter: 0,
 
   init : function() {
     //Initial View
@@ -50,21 +52,47 @@ var songs = {
     //Add song list
     _.each(data, function(song, index){
       $("#songListTable tr:last").after("<tr><td>" + song.id + "</td><td>" + song.title + "</td><td>" + song.artist + "</td><td>" + 
-        song.release_date + "</td><td>" + song.price + "</td><td><a href=\"#\" onclick=\"songs.deleteSong("+index+")\">Delete</a></td></tr>");
+        song.release_date + "</td><td>" + song.price + "</td><td><a href=\"#\" onclick=\"songs.editSong("+index+")\">Edit</a></td></td><td><a href=\"#\" onclick=\"songs.deleteSong("+index+")\">Delete</a></td></tr>");
     });
   },
 
   addSong : function() {
     var song = {};
-    song.id = this.songList.songs.length+1;
-    song.title = $("#songName").val();
-    song.artist = $("#songArtist").val();
-    song.release_date = $("#rdate").val();
-    song.price = $("#price").val();
-    this.songList.songs.push(song);
-    //Save song to server
+    if(this.editing) {
+      this.editing = false;
+      song.id = $("#songId").val();
+      song.title = $("#songName").val();
+      song.artist = $("#songArtist").val();
+      song.release_date = $("#rdate").val();
+      song.price = $("#price").val();
+      this.songList.songs.splice(song.id-1, 1, song);
+
+    } else {
+      song.id = this.songList.songs.length+1;
+      song.title = $("#songName").val();
+      song.artist = $("#songArtist").val();
+      song.release_date = $("#rdate").val();
+      song.price = $("#price").val();
+      this.songList.songs.push(song);
+    }
+
+    // //Save song to server
     songs.saveSong(this.songList.songs);
-    songs.createSongListView(this.songList.songs);
+    // songs.createSongListView(this.songList.songs);
+  },
+
+  editSong : function(index) {
+    this.editing = true;
+    var song = this.songList.songs[index];
+
+    $("#addSongDiv").show();
+    $("#songList").hide();
+
+    $("#songId").val(song.id);
+    $("#songName").val(song.title);
+    $("#songArtist").val(song.artist);
+    $("#rdate").val(song.release_date);
+    $("#price").val(song.price);
   },
 
   deleteSong : function(index) {
@@ -85,7 +113,23 @@ var songs = {
   },
 
   saveSongReturn : function(data) {
-    alert("Return Data : " + data);
+    songs.counter++;
+    if(songs.counter >=5) {
+      $("#messages").empty();
+      $("#messages").show();
+      $("#messages").append("There was a problem adding your song to the list.  Try again.");
+      // $("#app").hide();
+      songs.counter = 0;
+    } else {
+      $("#messages").empty();
+      $("#messages").show();
+      $("#messages").append("Successfully added song to list.");
+      // $("#app").hide();
+          //Save song to server
+      // songs.saveSong(this.songList.songs);
+      songs.createSongListView(this.songList.songs);
+
+    }
   }
 
 }
